74ac8fad35297fc65322ba118bf8e8e1fd88a5b7
MasuqaT <occar@hotmail.co.jp>
2015-08-15 21:46:40 +0900

    Modify CI setting

---
+++ BeforeBuild.ps1
@@ -0,0 +1,17 @@
+﻿$nuspec = (ls $env:APPVEYOR_BUILD_FOLDER -Recurse).Where{ $_.Extension -eq ".nuspec"} | Select -First 1
+[xml]$xml = Get-Content $nuspec.FullName
+$number = $xml.package.metadata.version
+$stage = $xml.package.metadata.stage
+if(-not [String]::IsNullOrEmpty($stage)){
+    $stage = "-" + $stage
+}
+
+$nupkgVersion = $number + $stage
+$assemblyVersion = $nupkgVersion + "__" + $env:APPVEYOR_BUILD_NUMBER
+
+$env:APPVEYOR_BUILD_VERSION = $assemblyVersion
+
+$xml.package.metadata.version = $nupkgVersion
+$xml.Save($nuspec.FullName)
+
+rd "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Amazon Web Services LLC" /s /q
