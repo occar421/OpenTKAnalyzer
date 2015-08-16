7fb342fc1e358a8296ea7f5d437ee9d33128dd3a
MasuqaT <occar@hotmail.co.jp>
2015-08-16 20:56:32 +0900

    Add GL.Begin and GL.End analyzer skeleton

---
+++ src/OpenTKAnalyzer/OpenTKAnalyzer/OpenTK/Graphics/OpenGL/BeginEndAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace OpenTKAnalyzer.OpenTK.Graphics.OpenGL
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	class BeginEndAnalyzer : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = "BeginEnd";
+
+		private const string Title = "GL.Begin and GL.End";
+		private const string MessageFormat = "{0} is missing.";
+		private const string Description = "";
+		private const string Category = "OpenTKAnalyzer:OpenGL";
+
+		internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+			id: DiagnosticId,
+			title: Title,
+			messageFormat: MessageFormat,
+			category: Category,
+			defaultSeverity: DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			description: Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterCodeBlockAction(Analyze);
+		}
+
+		private static void Analyze(CodeBlockAnalysisContext context)
+		{
+			// TODO: syntax walker with block
+		}
+	}
+}
