782d2a0965e5b2ff1d5e0dda27ab8ab55d0d4d50
MasuqaT <occar@hotmail.co.jp>
2015-08-17 20:13:53 +0900

    Add Rotation value(OpenTK Math) analyzer skeleton

---
+++ src/OpenTKAnalyzer/OpenTKAnalyzer/OpenTK/RotationValueAnalyzer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace OpenTKAnalyzer.OpenTK
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	class BeginEndAnalyzer : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = "RotatinoValue";
+
+		private const string Title = "Rotation value(OpenTK Math)";
+		private const string MessageFormat = "{0} accepts radian values.";
+		private const string Description = "Warm on literal in argument seems degree.";
+		private const string Category = "OpenTKAnalyzer:OpenTK";
+
+		internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+			id: DiagnosticId,
+			title: Title,
+			messageFormat: MessageFormat,
+			category: Category,
+			defaultSeverity: DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			description: Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.InvocationExpression);
+		}
+
+		private static void Analyze(SyntaxNodeAnalysisContext context)
+		{
+			var invotation = context.Node as InvocationExpressionSyntax;
+
+			// TODO: check method name and literal on argument
+		}
+	}
+}
