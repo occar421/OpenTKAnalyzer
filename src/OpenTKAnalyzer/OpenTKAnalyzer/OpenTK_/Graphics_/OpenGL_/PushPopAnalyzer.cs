7c14c00a06124f5800dc3a4c22ffb99d65618aef
MasuqaT <occar@hotmail.co.jp>
2015-08-20 22:47:53 +0900

    Add GL.Push??? and GL.Pop??? analyzer skeleton

---
+++ src/OpenTKAnalyzer/OpenTKAnalyzer/OpenTK_/Graphics_/OpenGL_/PushPopAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenTKAnalyzer.OpenTK_.Graphics_.OpenGL_
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	class PushPopAnalyzer : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = "PushPop";
+
+		private const string Title = "GL.Push??? and GL.Pop??? comformity";
+		private const string MessageFormat = "Missing {0}.";
+		private const string Description = "Analyzer for GL.Push??? and GL.Push???.";
+		private const string Category = nameof(OpenTKAnalyzer) + ":" + nameof(OpenTK.Graphics.OpenGL);
+
+		private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+			id: DiagnosticId,
+			title: Title,
+			messageFormat: MessageFormat,
+			category: Category,
+			defaultSeverity: DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			description: Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterCodeBlockAction(Analyze);
+		}
+
+		private static void Analyze(CodeBlockAnalysisContext context)
+		{
+			// TODO
+		}
+	}
+}
