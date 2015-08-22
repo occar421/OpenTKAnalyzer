66dca119db1fcab6c7cf915fde5bec807a3aea56
MasuqaT <occar@hotmail.co.jp>
2015-08-22 16:22:13 +0900

    Add GL.Enable Lighting analyzer skeleton

---
+++ src/OpenTKAnalyzer/OpenTKAnalyzer/OpenTK_/Graphics_/OpenGL_/EnableLighting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using OpenTK.Graphics.OpenGL;
+
+
+namespace OpenTKAnalyzer.OpenTK_.Graphics_.OpenGL_
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	class EnableLighting : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = "EnableLighting";
+
+		private const string Title = "GL.Enable for lighting reminder.";
+		private const string MessageFormat = "Missing {0}.";
+		private const string Description = "Warm on forget whole lighting enabling.";
+		private const string Category = nameof(OpenTKAnalyzer) + ":" + nameof(OpenTK.Graphics.OpenGL);
+
+
+		private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+			id: DiagnosticId,
+			title: Title,
+			messageFormat: MessageFormat,
+			category: Category,
+			defaultSeverity: DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			description: Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterSemanticModelAction(Analyze);
+		}
+
+		private static async void Analyze(SemanticModelAnalysisContext context)
+		{
+			var root = await context.SemanticModel.SyntaxTree.GetRootAsync();
+
+			// (including  using static LightName; using static EnableCap;)
+			// GL.Light(LightName.Light?)
+			// => GL.Enable(EnableCap.Light?)
+			// => GL.Enable(EnableCap.Lighting)
+		}
+	}
+}
