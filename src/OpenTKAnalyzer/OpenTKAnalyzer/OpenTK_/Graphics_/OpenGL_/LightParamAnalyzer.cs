e8fd02ab3fdb83f8fea13657882c413c4516ee35
MasuqaT <occar@hotmail.co.jp>
2015-08-26 20:40:43 +0900

    Make analyzer public

--- src/OpenTKAnalyzer/OpenTKAnalyzer/OpenTK_/Graphics_/OpenGL_/LightParamAnalyzer.cs
+++ src/OpenTKAnalyzer/OpenTKAnalyzer/OpenTK_/Graphics_/OpenGL_/LightParamAnalyzer.cs
@@ -11,7 +11,7 @@ using OpenTK.Graphics.OpenGL;
 namespace OpenTKAnalyzer.OpenTK_.Graphics_.OpenGL_
 {
 	[DiagnosticAnalyzer(LanguageNames.CSharp)]
-	class LightParamAnalyzer : DiagnosticAnalyzer
+	public class LightParamAnalyzer : DiagnosticAnalyzer
 	{
 		public const string DiagnosticId = "LightParam";
 
