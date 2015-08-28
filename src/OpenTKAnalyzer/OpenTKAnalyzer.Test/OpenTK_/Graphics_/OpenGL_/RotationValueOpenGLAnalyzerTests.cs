2fd2a18fe67d68050358a7b51b7aa41882ff82f9
MasuqaT <occar@hotmail.co.jp>
2015-08-28 19:47:48 +0900

    Add tests for RotationValue(OpenGL) anlyzer

---
+++ src/OpenTKAnalyzer/OpenTKAnalyzer.Test/OpenTK_/Graphics_/OpenGL_/RotationValueOpenGLAnalyzerTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenTKAnalyzer.TestHelper;
+using Microsoft.CodeAnalysis;
+
+namespace OpenTKAnalyzer.OpenTK_.Graphics_.OpenGL_.Tests
+{
+	[TestClass]
+	public class RotationValueOpenGLAnalyzerTests : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer CSharpDiagnosticAnalyzer => new RotationValueOpenGLAnalyzer();
+
+		[TestMethod]
+		public void EmptyCode()
+		{
+			var test0Source = @"";
+
+			VerifyCSharpDiagnostic(test0Source);
+		}
+
+		[TestMethod]
+		public void NormalScenario()
+		{
+			var test0Source = @"using OpenTK.Graphics.OpenGL;
+class Class1
+{
+	void Method1()
+	{
+		GL.Rotate(10f, OpenTK.Vector3.One);
+		GL.Rotate(10f, 1.0f, 1.0f, 1.0f);
+	}
+}";
+
+			VerifyCSharpDiagnostic(test0Source);
+		}
+
+		[TestMethod]
+		public void IncorrectScenario()
+		{
+			var test0Source = @"using OpenTK.Graphics.OpenGL;
+class Class1
+{
+	void Method1()
+	{
+		GL.Rotate(1.0f, OpenTK.Vector3.One);
+		GL.Rotate(1.0f, 1.0f, 1.0f, 1.0f);
+	}
+}";
+
+			VerifyCSharpDiagnostic(test0Source,
+				new DiagnosticResult()
+				{
+					Id = RotationValueOpenGLAnalyzer.DiagnosticId,
+					Message = "GL.Rotate accepts degree value.",
+					Severity = DiagnosticSeverity.Info,
+					Locations = new[] { new DiagnosticResultLocation("Test0.cs", 6, 13) }
+				},
+				new DiagnosticResult()
+				{
+					Id = RotationValueOpenGLAnalyzer.DiagnosticId,
+					Message = "GL.Rotate accepts degree value.",
+					Severity = DiagnosticSeverity.Info,
+					Locations = new[] { new DiagnosticResultLocation("Test0.cs", 7, 13) }
+				});
+		}
+	}
+}
