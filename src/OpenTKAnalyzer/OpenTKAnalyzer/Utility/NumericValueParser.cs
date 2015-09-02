babf2d69e2c146ec957f32f0ca900b1fa1f1f414
MasuqaT <occar@hotmail.co.jp>
2015-09-02 19:53:03 +0900

    Extract parse process to a method

---
+++ src/OpenTKAnalyzer/OpenTKAnalyzer/Utility/NumericValueParser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTKAnalyzer.Utility
+{
+	static public class NumericValueParser
+	{
+		static public bool TryParseFromExpression(ExpressionSyntax expression, out double result)
+		{
+			var literalString = string.Empty;
+			if (expression is PrefixUnaryExpressionSyntax)
+			{
+				var s = expression as PrefixUnaryExpressionSyntax;
+				literalString = s.OperatorToken.ValueText + (s.Operand as LiteralExpressionSyntax)?.Token.ValueText;
+			}
+			else if (expression is LiteralExpressionSyntax)
+			{
+				var s = expression as LiteralExpressionSyntax;
+				literalString = s.Token.ValueText;
+			}
+			return double.TryParse(literalString, out result);
+		}
+	}
+}
