1957789b292fefc3dbdf61ce9c91883a53aac3be
MasuqaT <occar@hotmail.co.jp>
2015-09-03 22:05:01 +0900

    Add tests for parser

--- src/OpenTKAnalyzer/OpenTKAnalyzer.Test/Utility/NumericValueParserTests.ParseDouble.cs
+++ src/OpenTKAnalyzer/OpenTKAnalyzer.Test/Utility/NumericValueParserTests.ParseDouble.cs
@@ -5,6 +5,8 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxKind;
+using System.Collections.Generic;
+using System;
 
 namespace OpenTKAnalyzer.Utility.Tests
 {
@@ -13,30 +15,81 @@ namespace OpenTKAnalyzer.Utility.Tests
 		[TestClass]
 		public class ParseDouble
 		{
+			struct Pair
+			{
+				public double Correct { get; }
+				public ExpressionSyntax Expression { get; }
+
+				public Pair(double correctValue, ExpressionSyntax expression)
+				{
+					Correct = correctValue;
+					Expression = expression;
+				}
+			}
+
+			readonly IEnumerable<Pair> GoodSamples = new[] {
+				// 1
+				new Pair(1, LiteralExpression(NumericLiteralExpression, Literal(1))),
+
+				// 2.3
+				new Pair(2.3, LiteralExpression(NumericLiteralExpression, Literal(2.3))),
+
+				// 2.3f
+				new Pair(2.3, LiteralExpression(NumericLiteralExpression, Literal("2.3f", 2.3f))),
+
+				// +1
+				new Pair(1, PrefixUnaryExpression(UnaryPlusExpression, LiteralExpression(NumericLiteralExpression, Literal(1)))),
+
+				// +2.3
+				new Pair(2.3, PrefixUnaryExpression(UnaryPlusExpression, LiteralExpression(NumericLiteralExpression, Literal(2.3)))),
+
+				// -1
+				new Pair(-1, PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1)))),
+
+				// -2.3
+				new Pair(-2.3, PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(2.3)))),
+
+				// (1)
+				new Pair(1, ParenthesizedExpression(LiteralExpression(NumericLiteralExpression, Literal(1)))),
+
+				// (2.3)
+				new Pair(2.3, ParenthesizedExpression(LiteralExpression(NumericLiteralExpression, Literal(2.3)))),
+
+				// (-1)
+				new Pair(-1, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1))))),
+
+				// (-2.3)
+				new Pair(-2.3, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(2.3))))),
+
+				// -(-1)
+				new Pair(1, PrefixUnaryExpression(UnaryMinusExpression, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1)))))),
+
+				// -(-2.3)
+				new Pair(2.3, PrefixUnaryExpression(UnaryMinusExpression, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(2.3))))))
+			};
+
+			readonly IEnumerable<ExpressionSyntax> BadExpressions = new ExpressionSyntax[]
+			{
+				// "num"
+				LiteralExpression(StringLiteralExpression, Literal("num")),
+
+				// name
+				IdentifierName("name"),
+
+				// -name
+				PrefixUnaryExpression(UnaryMinusExpression, IdentifierName("name")),
+
+				// (name)
+				ParenthesizedExpression(IdentifierName("name")),
+
+				// 1+1
+				BinaryExpression(AddExpression, LiteralExpression(NumericLiteralExpression, Literal(1)), LiteralExpression(NumericLiteralExpression, Literal(1)))
+			};
+
 			[TestMethod]
 			public void TryParseFromExpression_NormalScenario()
 			{
-				var samples = new[] {
-					// 1
-					new Pair(1, LiteralExpression(NumericLiteralExpression, Literal(1))),
-
-					// +1
-					new Pair(1, PrefixUnaryExpression(UnaryPlusExpression, LiteralExpression(NumericLiteralExpression, Literal(1)))),
-
-					// -1
-					new Pair(-1, PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1)))),
-
-					// (1)
-					new Pair(1, ParenthesizedExpression(LiteralExpression(NumericLiteralExpression, Literal(1)))),
-
-					// (-1)
-					new Pair(-1, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1))))),
-
-					// -(-1)
-					new Pair(1, PrefixUnaryExpression(UnaryMinusExpression, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1))))))
-				};
-
-				foreach (var sample in samples)
+				foreach (var sample in GoodSamples)
 				{
 					double result;
 					Assert.IsTrue(NumericValueParser.TryParseFromExpression(sample.Expression, out result), $"parse error on \"{sample.Expression.ToFullString()}\"");
@@ -47,40 +100,57 @@ namespace OpenTKAnalyzer.Utility.Tests
 			[TestMethod]
 			public void TryParseFromExpression_IncorrectScenario()
 			{
-				var samples = new ExpressionSyntax[]
-				{
-					// "num"
-					LiteralExpression(StringLiteralExpression, Literal("num")),
-
-					// name
-					IdentifierName("name"),
-
-					// -name
-					PrefixUnaryExpression(UnaryMinusExpression, IdentifierName("name")),
-
-					// (name)
-					ParenthesizedExpression(IdentifierName("name")),
-
-					// 1+1
-					BinaryExpression(AddExpression, LiteralExpression(NumericLiteralExpression, Literal(1)), LiteralExpression(NumericLiteralExpression, Literal(1)))
-				};
-
-				foreach (var expression in samples)
+				foreach (var expression in BadExpressions)
 				{
 					double result;
-					Assert.IsFalse(NumericValueParser.TryParseFromExpression(expression, out result), $"parse mistakenly passed on \"{expression.ToFullString()}\"");
+					Assert.IsFalse(NumericValueParser.TryParseFromExpression(expression, out result), $"incorrect result : {result} | parse mistakenly passed on \"{expression.ToFullString()}\"");
 				}
 			}
 
-			struct Pair
+			[TestMethod]
+			public void ParseFromExpressionDoubleOrNull_NormalScenario()
 			{
-				public double Correct { get; }
-				public ExpressionSyntax Expression { get; }
-
-				public Pair(double correctValue, ExpressionSyntax expression)
+				foreach (var sample in GoodSamples)
 				{
-					Correct = correctValue;
-					Expression = expression;
+					var result = NumericValueParser.ParseFromExpressionDoubleOrNull(sample.Expression);
+					Assert.AreEqual(result, sample.Correct, $"expected {sample.Correct} but {result} | on \"{sample.Expression.ToFullString()}\"");
+				}
+			}
+
+			[TestMethod]
+			public void ParseFromExpressionDoubleOrNull_IncorrectScenario()
+			{
+				foreach (var expression in BadExpressions)
+				{
+					var result = NumericValueParser.ParseFromExpressionDoubleOrNull(expression);
+					Assert.IsNull(result, $"incorrect result : {result} | parse mistakenly passed on \"{expression.ToFullString()}\"");
+				}
+			}
+
+			[TestMethod]
+			public void ParseFromExpressionDouble_NormalScenario()
+			{
+				foreach (var sample in GoodSamples)
+				{
+					var result = NumericValueParser.ParseFromExpressionDouble(sample.Expression);
+					Assert.AreEqual(result, sample.Correct, $"expected {sample.Correct} but {result} | on \"{sample.Expression.ToFullString()}\"");
+				}
+			}
+
+			[TestMethod]
+			public void ParseFromExpressionDouble_IncorrectScenario()
+			{
+				foreach (var expression in BadExpressions)
+				{
+					try
+					{
+						var result = NumericValueParser.ParseFromExpressionDouble(expression);
+						Assert.Fail($"incorrect result : {result} | parse mistakenly passed on \"{expression.ToFullString()}\"");
+					}
+					catch (FormatException ex)
+					{
+						// ok
+					}
 				}
 			}
 		}
--- src/OpenTKAnalyzer/OpenTKAnalyzer.Test/Utility/NumericValueParserTests.ParseInt.cs
+++ src/OpenTKAnalyzer/OpenTKAnalyzer.Test/Utility/NumericValueParserTests.ParseInt.cs
@@ -5,6 +5,8 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxKind;
+using System.Collections.Generic;
+using System;
 
 namespace OpenTKAnalyzer.Utility.Tests
 {
@@ -13,30 +15,81 @@ namespace OpenTKAnalyzer.Utility.Tests
 		[TestClass]
 		public class ParseInt
 		{
+			struct Pair
+			{
+				public int Correct { get; }
+				public ExpressionSyntax Expression { get; }
+
+				public Pair(int correctValue, ExpressionSyntax expression)
+				{
+					Correct = correctValue;
+					Expression = expression;
+				}
+			}
+
+			readonly IEnumerable<Pair> GoodSamples = new[] {
+				// 1
+				new Pair(1, LiteralExpression(NumericLiteralExpression, Literal(1))),
+
+				// +1
+				new Pair(1, PrefixUnaryExpression(UnaryPlusExpression, LiteralExpression(NumericLiteralExpression, Literal(1)))),
+
+				// -1
+				new Pair(-1, PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1)))),
+
+				// (1)
+				new Pair(1, ParenthesizedExpression(LiteralExpression(NumericLiteralExpression, Literal(1)))),
+
+				// (-1)
+				new Pair(-1, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1))))),
+
+				// -(-1)
+				new Pair(1, PrefixUnaryExpression(UnaryMinusExpression, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1)))))),
+			};
+
+			readonly IEnumerable<ExpressionSyntax> BadExpressions = new ExpressionSyntax[]
+			{
+				// "num"
+				LiteralExpression(StringLiteralExpression, Literal("num")),
+
+				// name
+				IdentifierName("name"),
+
+				// -name
+				PrefixUnaryExpression(UnaryMinusExpression, IdentifierName("name")),
+
+				// (name)
+				ParenthesizedExpression(IdentifierName("name")),
+
+				// 1+1
+				BinaryExpression(AddExpression, LiteralExpression(NumericLiteralExpression, Literal(1)), LiteralExpression(NumericLiteralExpression, Literal(1))),
+
+				// 2.3
+				LiteralExpression(NumericLiteralExpression, Literal(2.3)),
+
+				// 2.3f
+				LiteralExpression(NumericLiteralExpression, Literal("2.3f", 2.3f)),
+
+				// +2.3
+				PrefixUnaryExpression(UnaryPlusExpression, LiteralExpression(NumericLiteralExpression, Literal(2.3))),
+
+				// -2.3
+				PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(2.3))),
+
+				// (2.3)
+				ParenthesizedExpression(LiteralExpression(NumericLiteralExpression, Literal(2.3))),
+				
+				// (-2.3)
+				ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(2.3)))),
+
+				// -(-2.3)
+				PrefixUnaryExpression(UnaryMinusExpression, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(2.3)))))
+			};
+
 			[TestMethod]
 			public void TryParseFromExpression_NormalScenario()
 			{
-				var samples = new[] {
-					// 1
-					new Pair(1, LiteralExpression(NumericLiteralExpression, Literal(1))),
-
-					// +1
-					new Pair(1, PrefixUnaryExpression(UnaryPlusExpression, LiteralExpression(NumericLiteralExpression, Literal(1)))),
-
-					// -1
-					new Pair(-1, PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1)))),
-
-					// (1)
-					new Pair(1, ParenthesizedExpression(LiteralExpression(NumericLiteralExpression, Literal(1)))),
-
-					// (-1)
-					new Pair(-1, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1))))),
-
-					// -(-1)
-					new Pair(1, PrefixUnaryExpression(UnaryMinusExpression, ParenthesizedExpression(PrefixUnaryExpression(UnaryMinusExpression, LiteralExpression(NumericLiteralExpression, Literal(1))))))
-				};
-
-				foreach (var sample in samples)
+				foreach (var sample in GoodSamples)
 				{
 					int result;
 					Assert.IsTrue(NumericValueParser.TryParseFromExpression(sample.Expression, out result), $"parse error on \"{sample.Expression.ToFullString()}\"");
@@ -47,40 +100,57 @@ namespace OpenTKAnalyzer.Utility.Tests
 			[TestMethod]
 			public void TryParseFromExpression_IncorrectScenario()
 			{
-				var samples = new ExpressionSyntax[]
-				{
-					// "num"
-					LiteralExpression(StringLiteralExpression, Literal("num")),
-
-					// name
-					IdentifierName("name"),
-
-					// -name
-					PrefixUnaryExpression(UnaryMinusExpression, IdentifierName("name")),
-
-					// (name)
-					ParenthesizedExpression(IdentifierName("name")),
-
-					// 1+1
-					BinaryExpression(AddExpression, LiteralExpression(NumericLiteralExpression, Literal(1)), LiteralExpression(NumericLiteralExpression, Literal(1)))
-				};
-
-				foreach (var expression in samples)
+				foreach (var expression in BadExpressions)
 				{
 					int result;
-					Assert.IsFalse(NumericValueParser.TryParseFromExpression(expression, out result), $"parse mistakenly passed on \"{expression.ToFullString()}\"");
+					Assert.IsFalse(NumericValueParser.TryParseFromExpression(expression, out result), $"incorrect result : {result} | parse mistakenly passed on \"{expression.ToFullString()}\"");
 				}
 			}
 
-			struct Pair
+			[TestMethod]
+			public void ParseFromExpressionIntOrNull_NormalScenario()
 			{
-				public int Correct { get; }
-				public ExpressionSyntax Expression { get; }
-
-				public Pair(int correctValue, ExpressionSyntax expression)
+				foreach (var sample in GoodSamples)
 				{
-					Correct = correctValue;
-					Expression = expression;
+					var result = NumericValueParser.ParseFromExpressionIntOrNull(sample.Expression);
+					Assert.AreEqual(result, sample.Correct, $"expected {sample.Correct} but {result} | on \"{sample.Expression.ToFullString()}\"");
+				}
+			}
+
+			[TestMethod]
+			public void ParseFromExpressionIntOrNull_IncorrectScenario()
+			{
+				foreach (var expression in BadExpressions)
+				{
+					var result = NumericValueParser.ParseFromExpressionIntOrNull(expression);
+					Assert.IsNull(result, $"incorrect result : {result} | parse mistakenly passed on \"{expression.ToFullString()}\"");
+				}
+			}
+
+			[TestMethod]
+			public void ParseFromExpressionInt_NormalScenario()
+			{
+				foreach (var sample in GoodSamples)
+				{
+					var result = NumericValueParser.ParseFromExpressionInt(sample.Expression);
+					Assert.AreEqual(result, sample.Correct, $"expected {sample.Correct} but {result} | on \"{sample.Expression.ToFullString()}\"");
+				}
+			}
+
+			[TestMethod]
+			public void ParseFromExpressionInt_IncorrectScenario()
+			{
+				foreach (var expression in BadExpressions)
+				{
+					try
+					{
+						var result = NumericValueParser.ParseFromExpressionInt(expression);
+						Assert.Fail($"incorrect result : {result} | parse mistakenly passed on \"{expression.ToFullString()}\"");
+					}
+					catch (FormatException ex)
+					{
+						// ok
+					}
 				}
 			}
 		}
