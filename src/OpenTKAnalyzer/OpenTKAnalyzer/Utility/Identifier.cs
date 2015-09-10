2ca24225e245fe19062166aa330fa72536631a61
MasuqaT <occar@hotmail.co.jp>
2015-09-10 19:09:52 +0900

    Improve Identifier.GetVariableString

--- src/OpenTKAnalyzer/OpenTKAnalyzer/Utility/Identifier.cs
+++ src/OpenTKAnalyzer/OpenTKAnalyzer/Utility/Identifier.cs
@@ -16,38 +16,76 @@ namespace OpenTKAnalyzer.Utility
 		/// </summary>
 		/// <param name="expression">expression node</param>
 		/// <param name="semanticModel">semantic Model</param>
-		/// <returns>indentity string</returns>
+		/// <returns>indentity string : [Namespace].[Class].[MemberName] or ([Namespace].[Class].[Method]).[LocalVariableName]</returns>
 		static public string GetVariableString(ExpressionSyntax expression, SemanticModel semanticModel)
 		{
+			if (semanticModel == null)
+			{
+				throw new ArgumentNullException(nameof(semanticModel));
+			}
 			if (expression == null)
 			{
 				return null;
 			}
-			if (expression is ElementAccessExpressionSyntax)
+			var walker = new NamingWalker(semanticModel);
+			walker.Visit(expression);
+			return walker.Identity;
+		}
+
+		class NamingWalker : CSharpSyntaxWalker
+		{
+			SemanticModel semanticModel;
+			string prefix = string.Empty;
+			string id = string.Empty;
+			string suffix = string.Empty;
+
+			public string Identity => string.IsNullOrEmpty(id) ? null : prefix + id + suffix;
+
+			public NamingWalker(SemanticModel semanticModel)
 			{
-				var identifierSymbol = semanticModel.GetSymbolInfo(expression.ChildNodes().First()).Symbol;
-				var index = expression.ChildNodes().Skip(1).FirstOrDefault()?.WithoutTrivia();
-				return identifierSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat) + index.ToFullString();
+				this.semanticModel = semanticModel;
 			}
-			if (expression is BinaryExpressionSyntax)
+
+			public override void DefaultVisit(SyntaxNode node)
 			{
-				return null;
+				var symbol = semanticModel.GetSymbolInfo(node).Symbol;
+				if (symbol?.Kind == SymbolKind.Local)
+				{
+					string methodName = symbol.ContainingSymbol.Name;
+
+					var className = string.Empty;
+					for (var cs = symbol.ContainingType; cs != null; cs = cs.ContainingType)
+					{
+						className = cs.Name + "." + className;
+					}
+
+					var namespaceName = string.Empty;
+					for (var ns = symbol.ContainingNamespace; ns != null && ns.Name != string.Empty; ns = ns.ContainingNamespace)
+					{
+						namespaceName = ns.Name + "." + namespaceName;
+					}
+					id = $"({namespaceName}{className}{methodName}).{symbol.Name}";
+				}
+				else if (symbol?.Kind == SymbolKind.Method)
+				{
+					var str = symbol.ToString();
+					id = str.Substring(0, str.Length - 2);
+				}
+				else
+				{
+					id = symbol?.ToString();
+				}
 			}
-			if (expression is PostfixUnaryExpressionSyntax)
+
+			public override void VisitInvocationExpression(InvocationExpressionSyntax node)
 			{
-				return null;
+				// no code here
 			}
-			if (NumericValueParser.ParseFromExpressionDoubleOrNull(expression as ExpressionSyntax).HasValue) // constant
+
+			public override void VisitElementAccessExpression(ElementAccessExpressionSyntax node)
 			{
-				return null;
-			}
-			if (expression is PrefixUnaryExpressionSyntax)
-			{
-				return null;
-			}
-			else
-			{
-				return semanticModel.GetSymbolInfo(expression).Symbol?.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+				Visit(node.Expression);
+				suffix = node.ArgumentList.WithoutTrivia().ToFullString() + suffix;
 			}
 		}
 	}
