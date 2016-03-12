25dff4f3442df389ab7a4d140a302976faf750f9
MasuqaT <occar@hotmail.co.jp>
2016-03-12 16:00:10 +0900

    Add xUnit test for appveyor test

---
+++ src/OpenTKAnalyzer/OpenTKAnalyzer.Test/OpenTK_/HogeTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace OpenTKAnalyzer.Test.OpenTK_
+{
+	public class HogeTest
+	{
+		[Fact]
+		public void PassingTest()
+		{
+			Assert.Equal(4, 2 + 2);
+		}
+
+		[Fact]
+		public void FailingTest()
+		{
+			Assert.Equal(5, 2 + 2);
+		}
+	}
+}
